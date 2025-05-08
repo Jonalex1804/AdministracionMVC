9e33f8516143645de4ed606eccee0fa6aab21625
Jonathan <granja.jon25@gmail.com>
2025-05-07 20:49:44 -0500

    Configuracion para render

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -8,8 +9,15 @@ login_manager.login_view = 'auth.login'
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'f4c0f510ea3e495f6cda38511aae3491'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/flask_crud_deudas'
+
+    # Clave secreta
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'f4c0f510ea3e495f6cda38511aae3491')
+
+    # Cadena de conexión dinámica
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        'DATABASE_URL',
+        'mysql+pymysql://root:@localhost/flask_crud_deudas'
+    )
 
     db.init_app(app)
     login_manager.init_app(app)
